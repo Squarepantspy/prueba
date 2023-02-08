183ce39ac64e1ea21e2c1997c2cf28de776c3c17
squarepantspy <pgsantax@gmail.com>
2023-02-08 03:40:11 -0300

    este es mi commit

---
+++ src/components/About.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>Acerca de Pablo</div>
+  )
+}
+
+export default About
---
+++ src/components/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='bg'>
+        <h1>Este es mi primer JSX en linux</h1>
+    </div>
+  )
+}
+
+export default Home
